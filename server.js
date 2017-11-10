60193a4d241139fb96d6eaee7f87c2988e980252
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-11-09 23:37:44 -0500

    test

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.use(express.static('public'));
 app.get('/webhook', (req, res) => {
   // test
   res.send('Hello world');
-
-  // verify
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
-    console.log("Validating webhook works");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
+  //
+  // // verify
+  // if (req.query['hub.mode'] === 'subscribe' &&
+  //     req.query['hub.verify_token'] === process.env.VALIDATION_TOKEN) {
+  //   console.log("Validating webhook works");
+  //   res.status(200).send(req.query['hub.challenge']);
+  // } else {
+  //   console.error("Failed validation. Make sure the validation tokens match.");
+  //   res.sendStatus(403);
+  // }
 
 });
 
